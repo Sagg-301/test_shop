c441f02e4a3d9135772829c11e8c85795f164dc2
Sergio <sagg_301@outlook.com>
2020-03-17 18:52:17 -0400

    Cambio color de boton

--- button.js
+++ button.js
@@ -10,7 +10,7 @@ function start_flow(price) {
         price = price / 85000
     }
 
-    let newWin = window.open(`http://c2p.itssca.net:3201?price=${price}&currency=${currency}`, "Pagar", param);
+    let newWin = window.open(`http://localhost:3000?price=${price}&currency=${currency}`, "Pagar", param);
 }
 
 function render_button(id, price, currency) {
@@ -25,7 +25,7 @@ function render_button(id, price, currency) {
         display: inline-block;
         font-size: 16px;`
     var select_style = `
-        background-color: #2f7dfc;
+        background-color: lightslategray;
         border: none;
         color: white;
         padding: 15px 0px 15px 0px;
@@ -38,11 +38,13 @@ function render_button(id, price, currency) {
                     <div class="row">
                         <div class="col-ls-2">
                             <select id="currency" style='${select_style}'>
-                                <option value="$">$</option>
                                 <option value="BsS">BsS</option>
+                                <option value="$">$</option>
                                 <option value="€">€</option>
                             </select>
                         </div>
-                        <div class="col-ls-10"><button style='${styles}' onClick="start_flow(${price}, ${currency})"><img src="./img/logos/itssca2.png" alt="logo" width="50px" height="50px"> Pagar</button></div>
+                        <div class="col-ls-10" style="margin-left:5px">
+                            <button style='${styles}' onClick="start_flow(${price}, ${currency})"><img src="./img/logos/itssca2.png" alt="logo" width="50px" height="50px"> Pagar</button>
+                        </div>
                     </div>`)
 }
